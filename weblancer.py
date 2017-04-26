ca195ad89d6cd61285ca803c82a1292166aea4be
ildar2244 <ildar2244@gmail.com>
2017-04-26 23:41:54 +0300

    Создан файл для парсинга. Реализован запрос к сайту и возврат всего содержимого.

---
+++ weblancer.py
@@ -0,0 +1,13 @@
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+def main():
+    print(get_html('http://weblancer.net/projects'))
+
+if __name__ == '__main__':
+    main()
