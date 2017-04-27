f316aa803e8200b9d0745b1c6dc668b2a1310c03
ildar2244 <ildar2244@gmail.com>
2017-04-27 23:11:30 +0300

    Сделано: возможность получения проектов с сайта со всех страниц. Сохранение в файл CSV.

--- weblancer.py
+++ weblancer.py
@@ -1,10 +1,18 @@
 
+import csv
 import urllib.request
 from bs4 import BeautifulSoup
+
+BASE_URL = 'http://weblancer.net/jobs'
 
 def get_html(url):
     response = urllib.request.urlopen(url)
     return response.read()
+
+def get_page_count(html):
+    soup = BeautifulSoup(html)
+    pagination = soup.find('ul', class_='pagination')
+    return int(pagination.find_all('a')[-1].get('href')[12:])
 
 def parse(html):
     soup = BeautifulSoup(html)
@@ -13,18 +21,39 @@ def parse(html):
     projects = []  #хранилице проектов
 
     for row in table.find_all('div', class_='row')[1:]:
-        col_title = row.find_all('div', class_='col-sm-7 col-lg-8')
+        col_title = row.find('div', class_='col-sm-7 col-lg-8')
+        col_category = row.find('div', class_='col-xs-12 text-muted')
+        col_price = row.find('div', class_='col-sm-1 amount title')
 
         projects.append({
-            'title': col_title[0].a.text
+            'title': col_title.a.text,
+            'categories': [category.text for category in col_category.find_all('a', class_='text-muted')],
+            'price': col_price.text.strip()
         })
 
-    for project in projects:
-        print(project)
+    return projects
 
+def save(projects, path):
+    with open(path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(('Проекты', 'Категории', 'Цена'))
+
+        for project in projects:
+            writer.writerow((project['title'], ', '.join(project['categories']), project['price']))
 
 def main():
-    parse(get_html('http://weblancer.net/jobs'))
+    page_count = get_page_count(get_html(BASE_URL))
+    print('Всего найдено страниц %d' % page_count)
+
+    projects = []
+    projects.extend(parse(get_html(BASE_URL)))
+
+    for page in range(2, 5):
+        print('Парсинг %d%%' % (page / page_count * 100))
+        projects.extend(parse(get_html(BASE_URL + '?page=%d' % page)))
+
+    save(projects, 'projects.csv')
+
 
 if __name__ == '__main__':
     main()
