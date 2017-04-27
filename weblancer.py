ee10e1eac75e42cc53003a9269cae2c4091d1e50
ildar2244 <ildar2244@gmail.com>
2017-04-27 21:30:44 +0300

    Получаем проекты с сайта. Выбираем заголовки.

--- weblancer.py
+++ weblancer.py
@@ -6,8 +6,25 @@ def get_html(url):
     response = urllib.request.urlopen(url)
     return response.read()
 
+def parse(html):
+    soup = BeautifulSoup(html)
+    table = soup.find('div', class_='container-fluid cols_table show_visited')
+
+    projects = []  #хранилице проектов
+
+    for row in table.find_all('div', class_='row')[1:]:
+        col_title = row.find_all('div', class_='col-sm-7 col-lg-8')
+
+        projects.append({
+            'title': col_title[0].a.text
+        })
+
+    for project in projects:
+        print(project)
+
+
 def main():
-    print(get_html('http://weblancer.net/projects'))
+    parse(get_html('http://weblancer.net/jobs'))
 
 if __name__ == '__main__':
     main()
